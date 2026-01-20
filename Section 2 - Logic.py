db55b593bde5bf0caf3ad944175ebfda1b9e8490
tlc120 <tlc1@kent.ac.uk>
2026-01-20 13:51:40 +0000

    Finish Section 1

---
+++ Section 2 - Logic.py
@@ -0,0 +1,15 @@
+#1. Basic If Statement
+age = int(input(f"What is your age? "))
+
+if age >= 18:
+    print("You are an adult")
+
+else:
+    print("You are a child")
+
+#If-Else Statement
+import random
+
+secret_number = random.randint(1, 10)
+
+guess_number = int(input(f"Please  "))

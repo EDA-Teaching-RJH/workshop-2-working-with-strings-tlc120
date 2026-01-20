d13bc3605780fe83ac02d1552789e9ad3863471f
tlc120 <tlc1@kent.ac.uk>
2026-01-20 13:26:33 +0000

    FInish

--- partOne.py
+++ partOne.py
@@ -3,6 +3,7 @@ def main():
     myFunction(slow)
 
 def myFunction(text):
-  #Your code goes here.
+  text = text.replace(" ", "...")
+  print(text)
 
 main()
--- partThree.py
+++ partThree.py
@@ -1,14 +1,19 @@
 def main():
     pounds = pounds_to_float(input("How much was the meal? "))
     percent = percent_to_float(input("What percentage would you like to charge? "))
-    charge = pounds * percent
+    charge = (pounds * percent)
     print(f"Charge £{charge:.2f}")
 
 
 def pounds_to_float(d):
-    # TODO
+    d = d.replace("£", "")
+    print(d)
+    return(float(d))
+            
 
 def percent_to_float(p):
-    # TODO
+    p = float(p.replace("%", ""))
+    print(p / 100 )
+    return(float(p / 100))
 
 main()
